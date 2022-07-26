61d4d2be257f17fa8129c5b2d48535837effae67
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-26 16:16:35 -0300

    <chore>: <progesso na funcionalidade de em andamento e finalizado>

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
 import { Heading, HStack, IconButton, Text, useTheme, VStack } from 'native-base';
 import { SignOut } from 'phosphor-react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
 
 export function Home() {
+    const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+
     const { colors } = useTheme();
+
     return (
         <VStack flex={1} pb={6} bg="gray.700">
             <HStack
@@ -38,11 +41,15 @@ export function Home() {
                     <Filter
                         type="open"
                         title="em andamento"
+                        onPress={() => setStatusSelected ('open')}
+                        isActive={statusSelected === 'open'}
                     />
 
                     <Filter
                         type="closed"
                         title="finalizado"
+                        onPress={() => setStatusSelected ('closed')}
+                        isActive={ statusSelected === 'closed'}
                     />
                 </HStack>
             </VStack>
