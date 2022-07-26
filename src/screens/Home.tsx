6e37359dc1e0f50914cfaf92803b4440f98cd3f7
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-26 20:51:13 -0300

    <feat>: implementando funcionalidade do chamado FlatList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,23 @@
-import { Heading, HStack, IconButton, Text, useTheme, VStack, FlatList } from 'native-base';
+import { Center, FlatList, Heading, HStack, IconButton, Text, useTheme, VStack } from 'native-base';
 import { SignOut } from 'phosphor-react-native';
 import React, { useState } from 'react';
 import Logo from '../assets/logo_secondary.svg';
+import { ChatTeardropText } from 'phosphor-react-native';
 
-import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
+import { Filter } from '../components/Filter';
 import { Order, OrderProps } from '../components/Order';
 
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-    const [orders, setOrders] = useState<OrderProps[]>([{
-        id: '123',
-        patrimony: '123456',
-        when: '18/07/2022',
-        status: 'open'
-
-    }]);
+    const [orders, setOrders] = useState<OrderProps[]>([
+        {
+            id: '123',
+            patrimony: '123456',
+            when: '18/07/2022',
+            status: 'open'
+        }
+    ]);
 
     const { colors } = useTheme();
 
@@ -50,22 +52,33 @@ export function Home() {
                     <Filter
                         type="open"
                         title="em andamento"
-                        onPress={() => setStatusSelected ('open')}
+                        onPress={() => setStatusSelected('open')}
                         isActive={statusSelected === 'open'}
                     />
 
                     <Filter
                         type="closed"
                         title="finalizado"
-                        onPress={() => setStatusSelected ('closed')}
-                        isActive={ statusSelected === 'closed'}
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'}
                     />
                 </HStack>
 
-                <FlatList 
+                <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
                     renderItem={({ item }) => <Order data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{paddingBottom:100}}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40} />
+                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center" >
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizado'}
+                            </Text>
+                        </Center>
+                    )}
                 />
 
                 <Button title="Nova Solicitação" />
