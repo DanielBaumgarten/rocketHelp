fd942fa68a9d3ca8022f5f5ac465bf0067aa38b6
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-27 11:38:20 -0300

    <chore>: <ativando a funcção do incone de retornar a página anterior>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Heading, HStack, IconButton, useTheme, StyledProps } from 'native-base';
 import { CaretLeft } from 'phosphor-react-native';
 import React from 'react';
@@ -7,7 +8,14 @@ type Props = StyledProps & {
 }
 
 export function Header({ title, ...rest }: Props) {
+
     const { colors } = useTheme()
+    const navigation = useNavigation()
+
+    function handleGoBack(){
+      navigation.goBack();
+    }
+
   return (
     <HStack
      w="full"
@@ -20,6 +28,7 @@ export function Header({ title, ...rest }: Props) {
     >
         <IconButton
             icon={<CaretLeft color={colors.gray[200]} size={24} />}
+            onPress={handleGoBack}
         />
 
         <Heading color="gray.100" textAlign="center" fontSize="lg" flex={1} ml={-6}>
